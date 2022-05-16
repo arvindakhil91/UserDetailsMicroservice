4105a843ef07bf2f53b49ffa258d131bc4479f80
arvindakhil91 <38256498+arvindakhil91@users.noreply.github.com>
2022-05-16 17:20:27 +0530

    test5

--- src/main/java/com/demo/userDetailService/SwaggerConfiguration.java
+++ src/main/java/com/demo/userDetailService/SwaggerConfiguration.java
@@ -12,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 	public class SwaggerConfiguration { 
-		//test2
+		//test3
 	    @Bean
 	    public Docket api() { 
 	        return new Docket(DocumentationType.SWAGGER_2)  
